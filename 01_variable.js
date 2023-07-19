93e65d80c3f2066f8f1aa3064040c1aa92f180aa
shegardnyanesh <shegardnyanesh@gmail.com>
2023-07-20 00:51:28 +0530

    variable declare
    variable declare

---
+++ 01_variable.js
@@ -0,0 +1,10 @@
+var myName; // Variable Declaration
+myName = "Dnyanesh"; // Variable Assignment or Initialization
+
+console.log(myName);
+
+var city = "Pune"; // Variable Declaration and Initialization
+console.log(city);
+
+var pin; // Variable Declaration
+console.log(pin);
