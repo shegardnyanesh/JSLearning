8dfc63c96e9474dfc5e05abf81b25676c5540fb9
shegardnyanesh <shegardnyanesh@gmail.com>
2023-07-20 01:53:35 +0530

    Variable assign
    Variable assign

---
+++ 01_varAssign.js
@@ -0,0 +1,29 @@
+var myFullName;
+console.log("Before initialize : ",myFullName); //Before initialize
+
+myFullName="Dnyanesh Shegar";
+console.log("After initialize : ",myFullName);
+
+myFullName-"Dnyanesh R Shegar";
+console.log("After Update : ",myFullName);
+
+var myLove="I Love only JS"
+console.log(myLove);
+
+var firstName="Dnyanesh";
+console.log("firstName : ",firstName);
+var lastName ="Shegar";
+console.log("lastName : ",lastName);
+var age=27;
+console.log("Age : ",age);
+var add="Bavdhan Pune"
+console.log("Address : ",add);
+var pin=413301;
+console.log("Pincode : ",pin);
+var gender="Male";
+console.log("Gender : ",gender);
+var isMarried=true;
+console.log("IsMarried : ",isMarried);
+
+
+
