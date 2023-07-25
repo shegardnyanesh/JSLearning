e6981eb74ae2a5220ef6bf0af7f9d01af5a8352f
shegardnyanesh <shegardnyanesh@gmail.com>
2023-07-26 01:14:06 +0530

    Variable swap
    Variable swap

---
+++ 01_swapVariabes.js
@@ -0,0 +1,31 @@
+
+var num1=100;
+var num2=200;
+console.log('Before Swap=','num1-', num1, 'num2-', num2);
+var temp=num1;
+num1=num2;
+console.log(temp, num1, num2);
+num2=temp
+console.log('After Swap=', 'num1-', num1, 'num2-', num2);
+
+console.log('---------------------------------------------------------------------------');
+
+console.log('-------Swapping using 2 values -------');
+var sweety = "Sweety"
+var cutie = "Cutie"
+console.log( 'Before Swap', 'Sweety-', sweety, 'Cutie-', cutie );
+var name=sweety;
+sweety=cutie;
+cutie=name;
+console.log('After Swap', 'Sweety-', sweety, 'Cutie-',  cutie);
+console.log('---------------------------------------------------------------------------');
+console.log('-------Swapping using 3 values -------');
+var num1=100;
+var num2=200;
+var num3=300;
+console.log('Before Swap', 'num1-', num1, 'num2-', num2, 'num3-', num3);
+var stole=num1
+num1=num2
+num2=num3
+num3=stole
+console.log('After Swap-', 'num1-', num1, 'num2-', num2, 'num3-', num3);
