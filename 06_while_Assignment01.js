428648784a93feff53e532b60efbac222dd891bd
shegardnyanesh <shegardnyanesh@gmail.com>
2023-08-01 01:31:13 +0530

    While loop Assignement
    While loop Assignement

---
+++ 06_while_Assignment01.js
@@ -0,0 +1,14 @@
+//WAP to print numbers from 5 to 15 by incrementing 1
+var i=5;
+while (i<=15) {
+    console.log(i);
+    i++;
+}
+console.log(`----------------`);
+
+//WAP to print numbers from 50 to 40 by decrementing by 1
+var num=50;
+while (num>=40) {
+    console.log(num);
+    num--;
+}
